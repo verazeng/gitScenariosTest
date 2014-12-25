0ab23995ba72c2963a521a0601057e1246e75d57
vera zeng <zengheng810@gmail.com>
2014-12-25 19:48:03 +0800

    create project

---
+++ gitSceneriosTest/gitSceneriosTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitSceneriosTest
+//
+//  Created by Heng Zeng on 12/25/14.
+//  Copyright (c) 2014 thoughtworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitSceneriosTest/gitSceneriosTest/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  gitSceneriosTest
+//
+//  Created by Heng Zeng on 12/25/14.
+//  Copyright (c) 2014 thoughtworks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
